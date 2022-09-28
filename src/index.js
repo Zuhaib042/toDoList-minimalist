5487bfeba74cd86f850c78e7bffc3b04135fe0f3
Zuhaib Amjad <umar0423@gmail.com>
2022-09-28 19:36:51 +0500

    Add final touchings to styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import tasks, { inputAdd, listContainer } from './modules/list.js';
 
 window.onload = tasks.displayList();
-
 inputAdd.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     // key code of the keybord key
