6fdd2d3de13a32b6dd81d14305fe60aab4ff6262
Zuhaib Amjad <umar0423@gmail.com>
2022-09-28 18:45:46 +0500

    Implement the Add and Remove functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './style.css';
-import tasks from './modules/list.js';
+import tasks, { inputAdd, listContainer } from './modules/list.js';
 
-const listContainer = document.getElementById('container');
+window.onload = tasks.displayList();
+// const singleLi = document.querySelectorAll('.singleLi');
+// console.log(singleLi);
 
-const displayList = () => {
-  tasks.forEach((task) => {
-    const singleLiHtml = `<li class="singleLi" id="${task.index}">
-    <div class="checking">
-        <input type="checkbox" name="check">
-        ${task.decription}
-    </div>
-    <i class="fa-solid fa-ellipsis-vertical icon"></i>
-</li>`;
-    listContainer.insertAdjacentHTML('beforeend', singleLiHtml);
-  });
-};
-window.onload = displayList();
+inputAdd.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    // key code of the keybord key
+    event.preventDefault();
+    listContainer.innerHTML = '';
+    // your code to Run
+    tasks.addTask();
+    tasks.displayList();
+    inputAdd.value = '';
+  }
+});
--- src/modules/list.js
+++ src/modules/list.js
@@ -1,19 +1,56 @@
-const tasks = [
-  {
-    decription: 'Eat Breakfast',
-    completed: false,
-    index: 0,
-  },
-  {
-    decription: 'Learn Data Structures',
-    completed: false,
-    index: 1,
-  },
-  {
-    decription: 'Complete daily projects',
-    completed: false,
-    index: 2,
-  },
-];
+export const tasksItems = JSON.parse(localStorage.getItem('lion')) || [];
+export const inputAdd = document.getElementById('addTo');
+export const listContainer = document.getElementById('container');
 
-export default tasks;
+export default class Tasks {
+  constructor(description, complete, index) {
+    this.description = description;
+    this.complete = complete;
+    this.index = index;
+  }
+  static displayList = () => {
+    tasksItems.forEach((task, index) => {
+      const singleLiHtml = `<li class="singleLi" id="${index}">
+      <div class="checking">
+      <input type="checkbox" name="checkbox" />
+          <input type="text" class="task-input" value="${task.description}" >
+      </div>
+      <i class="fa-solid fa-trash-can icon delete"></i>
+  </li>`;
+      listContainer.insertAdjacentHTML('beforeend', singleLiHtml);
+    });
+    const deleteBtn = document.querySelectorAll('.delete');
+    deleteBtn.forEach((del, i) => {
+      del.addEventListener('click', () => {
+        Tasks.removeTask(i);
+      });
+    });
+    const taskInputs = document.querySelectorAll('.task-input');
+    console.log(taskInputs);
+    taskInputs.forEach((task, index) => {
+      task.addEventListener('change', () => {
+        if (task.value) {
+          tasksItems[index].description = task.value;
+          localStorage.setItem('lion', JSON.stringify(tasksItems));
+        }
+      });
+    });
+  };
+
+  static addTask = () => {
+    const task = new Tasks(inputAdd.value, false, tasksItems.length + 1);
+    if (inputAdd.value !== '') {
+      console.log(inputAdd.value);
+      tasksItems.push(task);
+      localStorage.setItem('lion', JSON.stringify(tasksItems));
+    }
+  };
+
+  static removeTask = (index) => {
+    tasksItems.splice(index, 1);
+    tasksItems.forEach((e, i) => (e.index = i + 1));
+    listContainer.innerHTML = '';
+    localStorage.setItem('lion', JSON.stringify(tasksItems));
+    Tasks.displayList();
+  };
+}
